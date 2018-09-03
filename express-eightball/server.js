103b4f5a874a89ba9f6bb64ebfe865a282285ee5
OttoHein <33087686+OttoHein@users.noreply.github.com>
2018-09-03 15:31:39 +0200

    Added existing quest check
    8ball checks for existing questions

--- express-eightball/server.js
+++ express-eightball/server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+var questions = [];
+
 // Redirect to the 8ball
 app.get('/', (req, res) => {
    res.redirect('/magicball');
@@ -18,5 +20,26 @@ app.get('/', (req, res) => {
 
 // Show the 8ball
 app.get('/magicball', (req, res) => {
-   res.render('magicball.ejs', { post: '' });
+   res.render('magicball.ejs', {question: '', answer: '' });
+});
+
+//Ask the question
+app.post('/ask', (req, res) => {
+  var random = Math.floor(Math.random() * 2);
+
+  questions.filter(function(question){
+    if(question.question == req.body.question){
+      res.render('magicball.ejs',{question: req.body.question, answer: "Question already asked: " + question.answer})
+    }
+  })
+
+  if (random == 0) {
+    var newQuestion = {question: req.body.question, answer: 'No'};
+    questions.push(newQuestion);
+    res.render('magicball.ejs',{question: req.body.question, answer: 'No'});
+  } else if(random == 1) {
+    var newQuestion = {question: req.body.question, answer: 'Yes'};
+    questions.push(newQuestion);
+    res.render('magicball.ejs',{question: req.body.question, answer: 'Yes'});
+  }
 });
